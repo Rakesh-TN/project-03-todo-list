2aaf04aef24e8fcbbb0ceb63605030d372d291f9
Rakesh TN <tnrakesh68@outlook.com>
2024-12-13 18:03:08 +0530

    Progress

---
+++ src/Components/Todo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Todo() {
+  return (
+    <div className='bg-black p-5 text-white card'>
+        <p className='text-4xl'>Todo List</p>
+    </div>
+  )
+}
